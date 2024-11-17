e7b16f9188c8cc18ca395a4834c37deb656c1160
Shakil <shakil.ahmed2@northsouth.edu>
2024-11-17 21:52:55 +0600

    files_organized

---
+++ Dashboard/db.php
@@ -0,0 +1,23 @@
+<?php  
+$servername = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$conn = new mysqli($servername, $username, $password); 
+  
+// Check connection 
+if ($conn->connect_error) { 
+    die("Connection failure: " 
+        . $conn->connect_error); 
+}  
+  
+// Creating a database named geekdata 
+$sql = "CREATE DATABASE geekdata"; 
+if ($conn->query($sql) === TRUE) { 
+    echo "Database with name geekdata"; 
+} else { 
+    echo "Error: " . $conn->error; 
+} 
+  
+// Closing connection 
+$conn->close(); 
+?> 
